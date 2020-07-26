2f3b99b4f9e921a3195d65b10240821880cc9fb8
Maksym Danylov <maxdanylov@gmail.com>
2020-07-26 23:01:57 +0300

    completed task 01 - 03

---
+++ js/task-02.js
@@ -0,0 +1,23 @@
+'use strict';
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const number = message.split(' ').length;
+  return number * pricePerWord;
+};
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+);
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+);
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+);
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+);
