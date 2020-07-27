ca7077f363eb336911d2a6f19d2d093d4b8fd808
Maksym Danylov <maxdanylov@gmail.com>
2020-07-28 01:20:51 +0300

    completed task 05

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
 'use strict';
+const formatString = function (string) {
+  if (string.length > 40) {
+    const splitString = string.split('');
+    splitString.length = 40;
+    const modifiedString = splitString.join('');
+    string = `${modifiedString}...`;
+  }
+
+  return string;
+};
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString('Curabitur ligula sapien.'));
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
