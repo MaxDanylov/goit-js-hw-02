6d3aebdb604268dbb8f397b5c64f4d1724df36ae
Maksym Danylov <maxdanylov@gmail.com>
2020-07-28 02:33:41 +0300

    completed task 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,14 @@
 'use strict';
+const checkForSpam = function (message) {
+  const messageAnswer =
+    message.toLowerCase().includes('spam') ||
+    message.toLowerCase().includes('sale');
+  return messageAnswer;
+};
+console.log(checkForSpam('Latest technology news'));
+
+console.log(checkForSpam('JavaScript weekly newsletter'));
+
+console.log(checkForSpam('Get best sale offers now!'));
+
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
