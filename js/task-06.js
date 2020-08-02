e15ca1d43830d0c2340c58479c5a962309ffed7e
Maksym Danylov <maxdanylov@gmail.com>
2020-08-03 01:26:59 +0300

    completed task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let input;
+const numbers = [];
+let total = 0;
+
+const calculator = function () {
+  do {
+    input = prompt('Введите число');
+
+    if (Number.isNaN(Number(input))) {
+      alert('Было введено не число, попробуйте еще раз');
+      continue;
+    }
+
+    numbers.push(input);
+  } while (input !== null);
+
+  for (let number of numbers) {
+    total += Number(number);
+  }
+  return console.log(total);
+};
+
+calculator();
